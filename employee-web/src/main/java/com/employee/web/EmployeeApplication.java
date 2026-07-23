6f0fcf5a407b0393893478f3a0c0b43f566b3d00
Rishith <rishith00@gmail.com>
2026-07-23 18:27:58 +0530

    Implemented scopes

--- employee-web/src/main/java/com/employee/web/EmployeeApplication.java
+++ employee-web/src/main/java/com/employee/web/EmployeeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import com.demo.DemoUtil;
 @SpringBootApplication(scanBasePackages = {
         "com.employee.web",
         "com.employee.service"
---
+++ employee-web/src/test/java/com/employee/EmployeeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.employee;
+
+import com.employee.web.EmployeeApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EmployeeApplication.class)
+class EmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
