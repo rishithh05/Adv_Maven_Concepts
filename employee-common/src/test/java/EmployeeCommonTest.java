5ffdad8d1f0e9b80a980ac1aedb983c97a10b698
Rishith <rishith00@gmail.com>
2026-07-24 06:45:36 +0530

    Addes test class

---
+++ employee-common/src/test/java/EmployeeCommonTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EmployeeCommonTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(10, 5 + 5);
+    }
+
+}
